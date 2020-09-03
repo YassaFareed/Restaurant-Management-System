042b7fcbf6d84457cb37050f1052c80303f30dd3
Yassa Fareed <yassa.fareed99@gmail.com>
2020-09-03 18:00:34 +0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+
+});
+
+$(document).ready(function(){
+$("#mycarousel").carousel( { interval: 2000 } );
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+});
+$('#reserveButton').click(function(){
+$('#reserveModal').modal('show');
+});
+ $('#loginButton').click(function(){
+$('#loginModal').modal('show');
+});
